67373e3e82b5d311a17e783bc0d00b28280c8f0a
JKiboi <anythingtechke@gmail.com>
2023-03-04 22:33:19 +0300

    next

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
+
 const config = {
-    development: {
-      backendUrl: 'http://localhost:5000'
-    },
-    production: {
-      backendUrl: 'https://mern-annelle-app-api.onrender.com'
-    }
-  };
-  
-  export default config[process.env.NODE_ENV || 'development'];
+  development: {
+    backendUrl: 'http://localhost:5000'
+  },
+  production: {
+    backendUrl: 'https://mern-annelle-app-api.onrender.com'
+  }
+};
+
+export default config[process.env.NODE_ENV || 'development'];
