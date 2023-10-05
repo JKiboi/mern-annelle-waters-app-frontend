35084885b6b68362c617aadca0ec98b91c299428
JKiboi <anythingtechke@gmail.com>
2023-10-05 22:35:11 +0300

    Navbar update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,63 @@
-
-import React from 'react';
-import "../styles/NavBar.css"
+import React, { useState } from 'react';
+import '../styles/NavBar.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <nav className="navbar">
-      <h1 className="navbar__brand">Annelle Waters</h1>
-      <ul className="navbar__menu">
+    <nav className={`navbar ${menuOpen ? 'mobile-open' : ''}`}>
+      <div className="navbar__container">
+        <h1 className="navbar__brand">Annelle Waters</h1>
+        <div className={`navbar__menu-toggle ${menuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <ul className={`navbar__menu ${menuOpen ? 'active' : ''}`}>
         <li className="navbar__item">
-          <Link to="/" className="navbar__link">Home</Link>
+          <Link to="/" className="navbar__link" onClick={closeMenu}>
+            Home
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/delivery-schedule" className="navbar__link">Delivery Schedule</Link>
+          <Link to="/delivery-schedule" className="navbar__link" onClick={closeMenu}>
+            Delivery Schedule
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/client-management" className="navbar__link">Client Management</Link>
+          <Link to="/client-management" className="navbar__link" onClick={closeMenu}>
+            Client Management
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/inventory-management" className="navbar__link">Inventory Management</Link>
+          <Link to="/inventory-management" className="navbar__link" onClick={closeMenu}>
+            Inventory Management
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/sales-tracking" className="navbar__link">Sales Tracking</Link>
+          <Link to="/sales-tracking" className="navbar__link" onClick={closeMenu}>
+            Sales Tracking
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/expenses-tracking" className="navbar__link">Expenses Tracking</Link>
+          <Link to="/expenses-tracking" className="navbar__link" onClick={closeMenu}>
+            Expenses Tracking
+          </Link>
         </li>
         <li className="navbar__item">
-          <Link to="/analytics" className="navbar__link">Analytics</Link>
+          <Link to="/analytics" className="navbar__link" onClick={closeMenu}>
+            Analytics
+          </Link>
         </li>
       </ul>
     </nav>
